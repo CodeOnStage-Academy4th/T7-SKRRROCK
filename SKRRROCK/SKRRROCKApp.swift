0ff5807d8bf2f4780b4ba3ad9454129b2c669744
Huigyun Jeong <topone03@gmail.com>
2025-08-08 20:17:06 +0900

    chore: setup project

---
+++ SKRRROCK/SKRRROCKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKRRROCKApp.swift
+//  SKRRROCK
+//
+//  Created by 정희균 on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SKRRROCKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
