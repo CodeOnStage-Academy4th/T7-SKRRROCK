8cd28e74bf7e013f8f457cac793ffea379d11538
jun <wnsgur4092@gmail.com>
2025-08-08 23:32:56 +0900

    feat: ShadowingRecord 및 TargetLearner 스위프트 데이터 모델 추가

---
+++ SKRRROCK/Model/ShadowingRecord.swift
@@ -0,0 +1,24 @@
+//
+//  ShadowingRecord.swift
+//  SKRRROCK
+//
+//  Created by Jun on 8/8/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class ShadowingRecord {
+    var id: UUID
+    var userName: String // 사용자 이름
+    var finalScore: Int
+    var targetLearner: TargetLearner // 어떤 러너의 웃음을 따라하였는가?
+    
+    init(id: UUID, userName: String, finalScore: Int, targetLearner: TargetLearner) {
+        self.id = UUID()
+        self.userName = userName
+        self.finalScore = finalScore
+        self.targetLearner = targetLearner
+    }
+}
