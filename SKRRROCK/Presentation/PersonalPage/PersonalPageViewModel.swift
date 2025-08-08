67cc05067e4da7f426ba9df4319741778292ab10
Huigyun Jeong <topone03@gmail.com>
2025-08-09 03:08:33 +0900

    feat: implement `PersonalPageView` UI

---
+++ SKRRROCK/Presentation/PersonalPage/PersonalPageViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalPageViewModel.swift
+//  SKRRROCK
+//
+//  Created by 정희균 on 8/9/25.
+//
+
+import SwiftUI
+
+protocol PersonalPageViewModel {
+  
+}
+
+@Observable
+final class DefaultPersonalPageViewModel: PersonalPageViewModel {
+  
+}
